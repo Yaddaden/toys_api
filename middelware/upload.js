3925dc138ddb8676906a0da3d772c94e20f35f3f
John Doe <rabahyad@gmail.com>
2023-06-21 02:34:29 +0200

    Suppression des images

--- middelware/upload.js
+++ middelware/upload.js
@@ -1,33 +1,3 @@
-// const multer = require("multer");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "images"); // Dossier où enregistrer les fichiers images
-//   },
-//   filename: (req, file, cb) => {
-//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     const extension = file.originalname.split(".").pop();
-//     cb(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   if (
-//     file.mimetype === "image/jpeg" ||
-//     file.mimetype === "image/png" ||
-//     file.mimetype === "image/jpg"
-//   ) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Invalid file type. Only JPEG, PNG, and JPG are allowed."));
-//   }
-// };
-
-// const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-// module.exports = upload;
-
-//-------------------------------essaie------------------------
 const multer = require("multer");
 
 const storage = multer.diskStorage({
--- routes/publication.js
+++ routes/publication.js
@@ -1,37 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const upload = require("../middelware/upload");
-// const publicationCtrl = require("../controllers/publication");
-// const auth = require("../middelware/auth");
-
-// //Route pour la création de l'annonce
-// router.post(
-//   "/",
-//   auth,
-//   upload.single("imageFile"),
-//   publicationCtrl.createPublication
-// );
-
-// // Route pour afficher toutes les publications
-// router.get("/", publicationCtrl.getAllPublication);
-
-// router.get("/:id", publicationCtrl.getOnePublication);
-
-// // modifications de la publication
-// router.put(
-//   "/:id",
-//   auth,
-//   upload.single("imageFile"),
-//   publicationCtrl.modifyPublication
-// );
-
-// // Suppression de la publication
-// router.delete("/:id", auth, publicationCtrl.deletePublication);
-
-// module.exports = router;
-
-//-----------------------------Essaie---------------------------
-
 const express = require("express");
 const router = express.Router();
 const upload = require("../middelware/upload");
