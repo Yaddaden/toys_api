5b8bf5997b8ef56e031c76b99a6772870c2b1a6f
John Doe <rabahyad@gmail.com>
2023-08-18 17:50:23 +0100

    Supprimer une image spécifique

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,222 +1,14 @@
-// //Création de la publication
-
-// const connection = require("../db/db.mysql");
-// const fs = require("fs");
-
-// exports.createPublication = (req, res) => {
-//   const { nom, marque, age, description, etat, email, telephone, prix } =
-//     req.body;
-
-//   const image = req.file; // Obtenez le fichier image téléchargé
-//   if (!req.auth.userId) {
-//     res
-//       .status(401)
-//       .json({ message: "tu n'est pas autorisé à créer l'annonce " });
-//   }
-//   const query =
-//     "INSERT INTO publications (nom, marque, age, description, etat, email, telephone, prix, image, userId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-//   connection.query(
-//     query,
-//     [
-//       nom,
-//       marque,
-//       age,
-//       description,
-//       etat,
-//       email,
-//       telephone,
-//       prix,
-//       image.filename,
-//       req.auth.userId,
-//     ],
-//     (err, results) => {
-//       if (err) {
-//         res.status(500).send("Error inserting publication into database");
-//       } else {
-//         res.status(200).json(results);
-//       }
-//     }
-//   );
-// };
-
-// //Affichage de toutes les annonces
-// exports.getAllPublication = (req, res) => {
-//   const query = "SELECT * FROM publications";
-//   connection.query(query, (err, results) => {
-//     if (err) {
-//       res.status(500).send("Error retrieving publications from database");
-//     } else {
-//       res.status(200).json(results);
-//     }
-//   });
-// };
-
-// exports.getOnePublication = (req, res) => {
-//   const query = "SELECT * FROM publications WHERE id = ?";
-//   connection.query(query, [req.params.id], (err, results) => {
-//     if (err) {
-//       res.status(500).send("Error retrieving publications from database");
-//     } else {
-//       res.status(200).json(results[0]);
-//     }
-//   });
-// };
-
-// //Modifier
-
-// exports.modifyPublication = (req, res) => {
-//   const { nom, marque, age, description, etat, email, telephone, prix } =
-//     req.body;
-
-//   const image = req.file;
-//   console.log(image);
-
-//   const publicationId = req.params.id;
-
-//   //requête pour vérifier si l'utilisateur est propriétaire de la publication
-//   const userId = req.auth.userId;
-//   console.log(userId);
-
-//   const checkOwnershipQuery =
-//     "SELECT * FROM publications WHERE id = ? AND userId = ?";
-//   connection.query(
-//     checkOwnershipQuery,
-//     [publicationId, userId],
-//     (error, results) => {
-//       console.log("erreurQuery =>", error);
-//       console.log("resultQuery =>", results);
-//       if (error) {
-//         res.status(500).send("Error checking publication ownership");
-//       } else if (results.length === 0) {
-//         res.status(401).send("Unauthorized to modify this publication");
-//       } else {
-//         // Supprimer l'ancienne image
-//         const getOldImageNameQuery =
-//           "SELECT image FROM publications WHERE id = ? AND userId = ?";
-//         connection.query(
-//           getOldImageNameQuery,
-//           [publicationId, userId],
-//           (error, results) => {
-//             if (error) {
-//               res.status(500).send("Error retrieving old image name");
-//             } else if (results.length > 0) {
-//               const oldImageName = results[0].image;
-//               deleteImage(oldImageName);
-//             }
-//           }
-//         );
-
-//         let query;
-//         let queryValues;
-//         if (image) {
-//           query =
-//             "UPDATE publications SET nom = ?, marque = ?, age = ?, description = ?, etat = ?, email = ?, telephone = ?, prix = ?, image = ? WHERE id = ? AND userId = ?";
-
-//           queryValues = [
-//             nom,
-//             marque,
-//             age,
-//             description,
-//             etat,
-//             email,
-//             telephone,
-//             prix,
-//             image.filename,
-//             publicationId,
-//             userId,
-//           ];
-//         } else {
-//           query =
-//             "UPDATE publications SET nom = ?, marque = ?, age = ?, description = ?, etat = ?, email = ?, telephone = ?, prix = ? WHERE id = ? AND userId = ?";
-//           queryValues = [
-//             nom,
-//             marque,
-//             age,
-//             description,
-//             etat,
-//             email,
-//             telephone,
-//             prix,
-//             publicationId,
-//             userId,
-//           ];
-//         }
-//         console.log(userId, publicationId),
-//           connection.query(query, queryValues, (err, results) => {
-//             if (err) {
-//               res.status(500).send("Error updating publication in database");
-//             } else {
-//               res.status(200).json(results);
-//             }
-//           });
-//       }
-//     }
-//   );
-// };
-
-// //Suppression
-// exports.deletePublication = (req, res) => {
-//   const publicationId = req.params.id;
-//   const userId = req.auth.userId;
-
-//   // Vérifier si l'utilisateur est propriétaire de la publication
-//   const checkOwnershipQuery =
-//     "SELECT * FROM publications WHERE id = ? AND userId = ?";
-//   connection.query(
-//     checkOwnershipQuery,
-//     [publicationId, userId],
-//     (error, results) => {
-//       if (error) {
-//         res.status(500).send("Error checking publication ownership");
-//       } else if (results.length === 0) {
-//         res.status(401).send("Unauthorized to delete this publication");
-//       } else {
-//         // L'utilisateur est propriétaire, supprimer la publication
-//         const deleteQuery = "DELETE FROM publications WHERE id = ?";
-//         connection.query(deleteQuery, [publicationId], (err, result) => {
-//           if (err) {
-//             res.status(500).send("Error deleting publication from database");
-//           } else {
-//             // Obtenir le nom du fichier image de la publication supprimée
-//             const imageName = results[0].image;
-//             res.status(200).json(result);
-
-//             // Appeler une fonction pour supprimer le fichier image du dossier backend
-//             deleteImage(imageName);
-//             console.log(imageName);
-//           }
-//         });
-//       }
-//     }
-//   );
-// };
-
-// //Suppression des images
-// const deleteImage = (imageName) => {
-//   const imagePath = `C:\\Users\\Pitch\\OneDrive\\Documents\\toys_api\\images\\${imageName}`;
-
-//   fs.unlink(imagePath, (err) => {
-//     if (err) {
-//       console.error("Erreur lors de la suppression du fichier image :", err);
-//     } else {
-//       console.log("Fichier image supprimé avec succès !");
-//     }
-//   });
-// };
-
-//----------------------------Essaie------------------------------
 //Création de la publication
 
 const connection = require("../db/db.mysql");
 const fs = require("fs");
-
+const path = require("path");
 exports.createPublication = (req, res) => {
   const { nom, marque, age, description, etat, email, telephone, prix } =
     req.body;
 
   const images = req.files; // Obtenez le fichier image téléchargé
-  //-----------------------------------
+
   const imagesPaths = [];
 
   images.forEach((image) => {
@@ -224,7 +16,6 @@ exports.createPublication = (req, res) => {
   });
 
   const imagesString = JSON.stringify(imagesPaths);
-  //-------------------------------------
 
   if (!req.auth.userId) {
     res
@@ -235,7 +26,7 @@ exports.createPublication = (req, res) => {
   if (!images || images.length === 0) {
     return res.status(400).json({ message: "Aucune image téléchargée." });
   }
-  //-------------------------------------------------
+
   // Vérifier le nombre d'images téléchargées
   const maxImageCount = 5;
   if (images.length > maxImageCount) {
@@ -243,7 +34,6 @@ exports.createPublication = (req, res) => {
       message: `Vous ne pouvez télécharger que ${maxImageCount} images.`,
     });
   }
-  //------------------------------------------------
 
   const query =
     "INSERT INTO publications (nom, marque, age, description, etat, email, telephone, prix, image, userId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
@@ -305,7 +95,7 @@ exports.modifyPublication = (req, res) => {
   console.log(image);
 
   const publicationId = req.params.id;
-  //----------------------------------------------
+
   // Vérifier le nombre d'images téléchargées
   const maxImageCount = 5; // Limite souhaitée
   if (image && image.length > maxImageCount) {
@@ -313,7 +103,6 @@ exports.modifyPublication = (req, res) => {
       message: `Vous ne pouvez télécharger que ${maxImageCount} images.`,
     });
   }
-  //---------------------------------------------------
 
   //requête pour vérifier si l'utilisateur est propriétaire de la publication
   const userId = req.auth.userId;
@@ -332,22 +121,6 @@ exports.modifyPublication = (req, res) => {
       } else if (results.length === 0) {
         res.status(401).send("Unauthorized to modify this publication");
       } else {
-        // Supprimer l'ancienne image
-        const getOldImageNameQuery =
-          "SELECT image FROM publications WHERE id = ? AND userId = ?";
-        connection.query(
-          getOldImageNameQuery,
-          [publicationId, userId],
-          (error, results) => {
-            if (error) {
-              res.status(500).send("Error retrieving old image name");
-            } else if (results.length > 0) {
-              const oldImageName = results[0].image;
-              deleteImage(oldImageName);
-            }
-          }
-        );
-
         let query;
         let queryValues;
         if (image && image.length > 0) {
@@ -414,18 +187,19 @@ exports.deletePublication = (req, res) => {
         res.status(401).send("Unauthorized to delete this publication");
       } else {
         // L'utilisateur est propriétaire, supprimer la publication
+
         const deleteQuery = "DELETE FROM publications WHERE id = ?";
         connection.query(deleteQuery, [publicationId], (err, result) => {
           if (err) {
             res.status(500).send("Error deleting publication from database");
           } else {
-            // Obtenir le nom du fichier image de la publication supprimée
-            const imageName = results[0].image;
-            res.status(200).json(result);
+            // Supprimer les images liées à la publication du dossier backend
+            const imageNamesArray = JSON.parse(results[0].image || "[]");
+            imageNamesArray.forEach((imageName) => {
+              deleteImage(imageName);
+            });
 
-            // Appeler une fonction pour supprimer le fichier image du dossier backend
-            deleteImage(imageName);
-            console.log(imageName);
+            res.status(200).json(result);
           }
         });
       }
@@ -433,9 +207,61 @@ exports.deletePublication = (req, res) => {
   );
 };
 
+// Supprimer une image spécifique d'une publication
+exports.deletePublicationImage = (req, res) => {
+  const publicationId = req.params.id;
+  const userId = req.auth.userId;
+  const imageName = req.body.imageName; // Le nom de l'image à supprimer
+  console.log("Image Name:", imageName);
+  // Vérifier si l'utilisateur est propriétaire de la publication
+  const checkOwnershipQuery =
+    "SELECT * FROM publications WHERE id = ? AND userId = ?";
+  connection.query(
+    checkOwnershipQuery,
+    [publicationId, userId],
+    (error, results) => {
+      if (error) {
+        res.status(500).send("Error checking publication ownership");
+      } else if (results.length === 0) {
+        res.status(401).send("Unauthorized to modify this publication");
+      } else {
+        // Récupérer les noms d'images depuis la base de données
+        const imageNamesArray = JSON.parse(results[0].image || "[]"); // Convertir en tableau
+        const updatedImageNamesArray = imageNamesArray.filter(
+          (name) => name !== imageName
+        );
+
+        // Supprimer le fichier image du dossier backend
+        deleteImage(imageName);
+
+        // Mettre à jour la base de données pour retirer le nom de l'image
+        const updateImageQuery =
+          "UPDATE publications SET image = ? WHERE id = ?";
+        connection.query(
+          updateImageQuery,
+          [JSON.stringify(updatedImageNamesArray), publicationId],
+          (err, result) => {
+            if (err) {
+              res.status(500).send("Error updating image in database");
+            } else {
+              res.status(200).json(result);
+            }
+          }
+        );
+      }
+    }
+  );
+};
 //Suppression des images
 const deleteImage = (imageName) => {
-  const imagePath = `C:\\Users\\Pitch\\OneDrive\\Documents\\toys_api\\images\\${imageName}`;
+  // Supprimer les crochets entourant le nom de l'image
+  const cleanImageName = imageName.replace(/[\[\]"]/g, "");
+
+  // Supprimer le premier élément "images\\" du chemin
+  const imagePathWithoutImages = cleanImageName.replace("images\\", "");
+
+  const imagePath = `./images/${imagePathWithoutImages}`;
+  console.log("Image Path:", imagePath);
 
   fs.unlink(imagePath, (err) => {
     if (err) {
